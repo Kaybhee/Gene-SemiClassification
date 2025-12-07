637c5deb0dcb2cdd78a9661a2834c5bb098be639
Kaybhee <abdulkabirbadru@gmail.com>
2025-12-07 22:50:57 +0100

    refactoring ports

--- app.py
+++ app.py
@@ -209,4 +209,6 @@ async def predict_csv(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    # uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
